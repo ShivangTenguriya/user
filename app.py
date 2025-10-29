23d3d056d210129fc66f064f8587f8e6380958a5
Shivang tenguriya <shivangtenguriya9897@gmail.com>
2025-10-29 17:28:31 +0530

    first update

--- app.py
+++ app.py
@@ -15,7 +15,6 @@ BASE_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
 if BASE_DIR not in sys.path:
     sys.path.insert(0, BASE_DIR)
 
-
 app = Flask(__name__)
 load_dotenv()
 app.secret_key = os.getenv('secret_key')
